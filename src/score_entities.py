6e754e5bb60c8eee23d357dba053531e8aef5b5b
Felix Labelle <flabelle@andrew.cmu.edu>
2020-04-25 12:22:41 +0000

    Further refined the skeleton, finished rough draft of Embedding Manager

--- src/score_entities.py
+++ src/score_entities.py
@@ -1,33 +1,65 @@
 import os
 import re
 from collections import namedtuple, defaultdict
-from lexicons import load_connotation_frames, load_power_verbs
+from lexicons import load_connotation_frames, load_power_verbs, load_hannah_split
 import h5py
 import ast
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
+
+# Custom types to handle embeddings and articles
+# TODO: Decide what information we really need. Putting
+# info specific to our code will be a pain in the ass
+# later on (if we try to use an external corpus)
+# NOTE: Assumes that the article_idx are unique over
+# a corpus.. Check this is a valid assumption
 Article = namedtuple("Article", "riot src article_id")
-CFParse = namedtuple("CFParse", "verb object subject")
+SentenceTuple = namedtuple("SentenceTuple", "article_idx sent_idx") 
+EmbeddingTuple = namedtuple("Embedding", "article_id sent_idx verb_idx word")
+
+def articleToName(article, append_str = ""):
+    """ Helper function that converts the 
+    article tuple into a str (e.g. filename)"""
+    return "_".join(article) + append_str
+
+
+# TODO: Add in words for keyword filtering
 # Consider making this dictionary specific to each conflict to improve performance
 keywords = {"Government": [],
             "Protestors": []}
 
-class EmbeddingsLUT:
-    def __init__(self):
-        self.verb_to_embeddings = defaultdict(list)
-        self.decontextualized_embeddings = {}
+# TODO: Implement a filter. Start by using the keywords defined above
+def filterSentence(sentence):
+    return True
 
-    def __getitem__(self, word):
-        return self.embeddings[word]
+
+# TODO: Complete the code and test
+# Class that houses embeddings and manage operations over them 
+class EmbeddingManager:
+    def __init__(self):
+        self.tuple_to_embeddings = {}
+        self.word_to_tuple = defaultdict(list)
+
+    def __getitem__(self, ref_tupl):
+        return self.tupl_to_embeddings[ref_tupl]
+    
+    def addItem(self, tupl, embedding):
+        self.tuple_to_embeddings[tupl] = embedding
+        self.word_to_tuple[tupl["word"]] += tupl
 
     def _calculateAverage(self, word):
-        return np.mean(self.__getitem__(word), axis=0)
+        word_embs = np.stack([self.__getitem__(tupl) for tupl in self.word_to_tuple[word]])
+        return np.mean(, axis=0)
 
     def decontextualize(self):
-        self.decontextualized_embeddings = {word : self._calculateAverage(word) for word in self.verb_to_embeddings}
+        return = {word : self._calculateAverage(word) for word in self.verb_to_embeddings}
 
 def getArticleList(dir_path, split_str="[_\.]"):
+    """ Function that loads all the files in a directory,
+    verifies their titles are correctly formatted,
+    returns them in a named tuple """
+
     article_list = []
     split_regex = re.compile(split_str)
     
@@ -40,21 +72,6 @@ def getArticleList(dir_path, split_str="[_\.]"):
         article_list.append(current_article)
     return article_list
 
-def articleToName(article, append_str = ""):
-    return "_".join(article) + append_str
-
-def sentenceToCFParse(sentence):
-    cf_parse = {}
-    for word in sentence:
-        if word.dep_ == "ROOT": #IS THE ROOT ALWAYS A VERb??
-            cf_parse['verb'] = word 
-        if word.dep_ == "dobj": #IS THE ROOT ALWAYS A VERb??
-            cf_parse['object'] = word
-        elif word.dep_ == "iobj": #IS THE ROOT ALWAYS A VERb??
-            cf_parse['object'] = word
-        if word.dep_ == "nsubj": #IS THE ROOT ALWAYS A VERb??
-            cf_parse['subject'] = word
-    return CFParse(**cf_parse)
 
 root_path = os.getcwd()
 article_path = os.path.join(root_path, "..", "our_articles")
@@ -62,27 +79,34 @@ data_path = os.path.join(root_path, "..", "our_training_data")
 # load articles
 articles = getArticleList(article_path)
 import pdb;pdb.set_trace()
-h5_file = os.path.join(data_path, articleToName(articles[0],append_str = ".txt.xml.hdf5"))
-#cf = load_connotation_frames()
+
+# NOTE: Initialize a dictionary that lets us go from (doc, sent_id) -> sentence
+sentences = {}
+
+# NOTE: Store embeddings according to (doc, sent_idx, verb_idx)
+# TODO: Finish this class. At it's core is a dictionary, but it
+# implements functionalities such as averaging
+embeddings = EmbeddingManager()
 
 # TODO: Repeat this for every article and store results in a dict
+h5_file = os.path.join(data_path, articleToName(articles[0],append_str = ".txt.xml.hdf5"))
 with h5py.File(h5_file, 'r') as h5py_file:
     sent_to_idx = ast.literal_eval(h5py_file.get("sentence_to_index")[0])
     idx_to_sent = {int(idx):sent for sent, idx in sent_to_idx.items()}
-    sentences = [''] * len(idx_to_sent)
     # This loop imitates the extract_entities and get_embeddings function in "match_parse.py"  
     for idx, sent in idx_to_sent.items():
         parsed_sentence = nlp(sent)
         # TODO: Filter sentences based on word content
-        # Below is a dummy example.E.G. (Storing sentenes in a fixed length array may not be ideal)
-        if filterFunction(parsed_sentence):
+        # These are to be used in the evaluation portion
+        # NOTE: This could be placed elsewhere
+        if filterSentence(parsed_sentence):
             sentences[idx] = parsed_sentence
-        else:
-            continue
-        # TODO: Extract the verbs in each sentence. Anjalie's code does a parse bassed on entities, but then does a second pass
-        # over the entire corpus to capture any missing verbs. Write this in the cf_parse function
-        cf_parse = sentenceToCFParse(parsed_sentence)
-        # CF parse replaces the following code
+
+        # TODO: Extract all the verbs in the sentence. Store the article, sent id, word, and embedding
+        # Anjalie's code does a parse bassed on entities, but then does a second pass
+        # over the entire corpus to capture any missing verbs. Write this in the cf_parse function 
+        # NOTE: We don't need the entities just yet. We can recover those from the filtered sentences
+        # NOTE: Work over the parsed_sentence, not the filtered ones.
         '''
                 sentence = root.find('document').find('sentences')[sent_id]
                 for dep in sentence.find('dependencies').iter('dep'):
@@ -105,7 +129,7 @@ with h5py.File(h5_file, 'r') as h5py_file:
                 name_to_verbs[name] += verbs_to_cache
 
         '''
-        # We will also need to keep track of all the verbs present
+        # Anjalie also keeps track of all the verbs present
         '''
         # Also keep all verbs that are in lex
         for s in root.find('document').find('sentences').iter('sentence'):
@@ -133,11 +157,17 @@ with h5py.File(h5_file, 'r') as h5py_file:
                                         s1[2][tupl.verb_id] * weights[2])
         '''
         # This is currently done by the get_token_representation in Line 391, representations.py
-        # We don't need most of this code, take a look at the EmbeddingsLUT class
 
 # TODO: Logistic regression
 # Logistic regression to predict the output using the decontextualized word embeddings
+#cf = load_connotation_frames()
+# NOTE: For now this will be done using Anjalie's code
 
+# NOTE: Is the paper_runs the comp Anjalie ran against the other papers?
+# Take the embeddings and average
+# load_hannah_split for each header
+# This fetches the test, train and dev splits in our connotation frames (???)
+# format_runs for each
 
 # TODO: Visualize and interpret results
 # Apply using word embeddings to rest of dataset
