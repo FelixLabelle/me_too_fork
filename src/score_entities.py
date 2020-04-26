cd5f3703901b23b53bb2a8f50436a617cd4aa03b
Felix Labelle <flabelle@andrew.cmu.edu>
2020-04-26 20:06:30 +0000

    Added latest version of code and env files

--- src/score_entities.py
+++ src/score_entities.py
@@ -6,10 +6,8 @@ from xml_helpers import process_xml_text
 from weighted_tests import run_connotation_frames
 import h5py
 import ast
-import spacy
 from tqdm import tqdm
 
-nlp = spacy.load("en_core_web_sm")
 
 # Custom types to handle embeddings and articles
 # TODO: Decide what information we really need. Putting
@@ -108,55 +106,60 @@ for article in tqdm(articles):
     try:
         h5_path = os.path.join(data_path, articleToName(article,append_str = ".txt.xml.hdf5"))
         xml_path = os.path.join(data_path, articleToName(article,append_str = ".txt.xml"))
-    except OSERROR:
-        print("Unable to read file {}".format(articleToName(article))
+    except OSError:
+        print("Unable to read file {}".format(articleToName(article)))
+        list_invalid_articles.append(article)
         continue
 
     root, document = process_xml_text(xml_path, correct_idx=False, stem=False, lower=True)
     sentence_nodes = [sent_node for sent_node in extractItem(root, ['document', 'sentences'], 'sentence')]
-    with h5py.File(h5_path, 'r') as h5py_file:
-        sent_to_idx = ast.literal_eval(h5py_file.get("sentence_to_index")[0])
-        idx_to_sent = {int(idx):sent for sent, idx in sent_to_idx.items()}
-        # TODO: Replace with logging
-        if len(sentence_nodes) != len(idx_to_sent):
-            print("Mismatch in number of sentences, {} vs {} for article {}. Skipping article".format(len(document), len(idx_to_sent), article))
-            list_invalid_articles.append(article)
-            continue
-        
-        for sent_idx in idx_to_sent:
-            sent_tokens = [tok for tok in  extractItem(sentence_nodes[sent_idx] , ['tokens'], 'token')]
-            sent_POS = [extractItem(tok, ['POS']).text for tok in sent_tokens]
-            sent_lemmas = [extractItem(tok, ['lemma']).text.lower() for tok in sent_tokens]
-            sent_embeddings = h5py_file.get(str(sent_idx))
-            
-            if sent_embeddings.shape[1] != len(sent_lemmas):
-                print("Mismatch in number of token in sentence {} : {} vs {}. Skipping sentence".format(sent_idx, sent_embeddings.shape[1], len(sent_lemmas)))
+    try:
+        with h5py.File(h5_path, 'r') as h5py_file:
+            sent_to_idx = ast.literal_eval(h5py_file.get("sentence_to_index")[0])
+            idx_to_sent = {int(idx):sent for sent, idx in sent_to_idx.items()}
+            # TODO: Replace with logging
+            if len(sentence_nodes) != len(idx_to_sent):
+                print("Mismatch in number of sentences, {} vs {} for article {}. Skipping article".format(len(document), len(idx_to_sent), article))
+                list_invalid_articles.append(article)
                 continue
-
-            # TODO: Filter sentences based on word content
-            # These are to be used in the evaluation portion
-            # NOTE: This could be placed elsewhere
-            if filterSentence(sent_lemmas):
-                # Check if there is a better way to index, we want to be easily able to remove types 
-                # of sentences (source, topic, etc..)
-                sentences[(article, sent_idx)] = sent_lemmas
-
-            # TODO: Extract all the verbs in the sentence. Store the article, sent id, word, and embedding
-            # Anjalie's code does a parse bassed on entities, but then does a second pass
-            # over the entire corpus to capture any missing verbs. Write this in the cf_parse function 
-            # NOTE: We don't need the entities just yet. We can recover those from the filtered sentences
-            # NOTE: Work over the parsed_sentence, not just the filtered ones.
             
-            # TODO: Look into Anjalie's paper and see if she discusses the weights
-            # Ditto for the elmo.py script
-            def retrieveWordEmbedding(sent_embedding, verb_idx, weights = [0,1,0]):
-                return sent_embedding[0][verb_idx] * weights[0] + sent_embedding[1][verb_idx] * weights[1] + sent_embedding[2][verb_idx] * weights[2]
+            for sent_idx in idx_to_sent:
+                sent_tokens = [tok for tok in  extractItem(sentence_nodes[sent_idx] , ['tokens'], 'token')]
+                sent_words = [extractItem(tok, ['word']).text.lower() for tok in sent_tokens]
+                sent_POS = [extractItem(tok, ['POS']).text for tok in sent_tokens]
+                sent_lemmas = [extractItem(tok, ['lemma']).text.lower() for tok in sent_tokens]
+                sent_embeddings = h5py_file.get(str(sent_idx))
+                
+                if sent_embeddings.shape[1] != len(sent_lemmas):
+                    print("Mismatch in number of token in sentence {} : {} vs {}. Skipping sentence".format(sent_idx, sent_embeddings.shape[1], len(sent_lemmas)))
+                    continue
 
-            for word_idx, (word,word_POS) in enumerate(zip(sent_POS, sent_lemmas)):
-                if word_POS == "VB":
-                    verb_embedding = EmbeddingTuple(article,sent_idx, word_idx, word, retrieveWordEmbedding(sent_embeddings, word_idx))
-                    training_embeddings.addItem(verb_embedding)
-            
+                # TODO: Filter sentences based on word content
+                # These are to be used in the evaluation portion
+                # NOTE: This could be placed elsewhere
+                if filterSentence(sent_lemmas):
+                    # Check if there is a better way to index, we want to be easily able to remove types 
+                    # of sentences (source, topic, etc..)
+                    sentences[(article, sent_idx)] = sent_lemmas
+
+                # TODO: Extract all the verbs in the sentence. Store the article, sent id, word, and embedding
+                # Anjalie's code does a parse bassed on entities, but then does a second pass
+                # over the entire corpus to capture any missing verbs. Write this in the cf_parse function 
+                # NOTE: We don't need the entities just yet. We can recover those from the filtered sentences
+                # NOTE: Work over the parsed_sentence, not just the filtered ones.
+                
+                # TODO: Look into Anjalie's paper and see if she discusses the weights
+                # Ditto for the elmo.py script
+                def retrieveWordEmbedding(sent_embedding, verb_idx, weights = [0,1,0]):
+                    return sent_embedding[0][verb_idx] * weights[0] + sent_embedding[1][verb_idx] * weights[1] + sent_embedding[2][verb_idx] * weights[2]
+
+                for word_idx, (word,word_POS) in enumerate(zip(sent_POS, sent_lemmas)):
+                    if word_POS == "VB":
+                        verb_embedding = EmbeddingTuple(article,sent_idx, word_idx, word, retrieveWordEmbedding(sent_embeddings, word_idx))
+                        training_embeddings.addItem(verb_embedding)
+    except OSError:
+        list_invalid_articles.append(article)
+        print("Invalid HDF5 file {}".format(articleToName(article)))
 
 import pdb;pdb.set_trace()
 # NOTE: Is the paper_runs the comp Anjalie ran against the other papers?
